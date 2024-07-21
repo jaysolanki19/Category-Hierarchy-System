518e8d4c8d8faaf91f78fafe60870279c18906f9
Jay Solanki <bholusolanki3411@gmail.com>
2024-07-21 15:33:52 +0530

    Redirection changes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('categories.index');
 });
 
 Route::resource('categories',CategoryController::class);
